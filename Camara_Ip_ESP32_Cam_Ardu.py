8427646d796972667e8d2378b3ec24a21d9de059
mAnU_MACHS <alejandromanu8@gmail.com>
2024-10-21 00:04:42 -0400

    Initial commit

---
+++ Camara_Ip_ESP32_Cam_Ardu.py
@@ -0,0 +1,85 @@
+import cv2
+import os
+import serial
+import time
+
+ser = serial.Serial('COM4', 9600, timeout=1)
+time.sleep(2)
+
+dataPath = 'C:/Users/manua/Desktop/ReconocimientoFacial/Data' #Cambia a la ruta donde hayas almacenado Data
+imagePaths = os.listdir(dataPath)
+print('imagePaths=',imagePaths)
+
+url='http://192.168.246.250/480x320.jpg'
+cap = cv2.VideoCapture(url) # Crear objeto VideoCapture
+
+winName = 'IP_CAM'
+cv2.namedWindow(winName, cv2.WINDOW_AUTOSIZE)
+
+#face_recognizer = cv2.face.EigenFaceRecognizer_create()
+#face_recognizer = cv2.face.FisherFaceRecognizer_create()
+face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+# Leyendo el modelo
+#face_recognizer.read('modeloEigenFace.xml')
+#face_recognizer.read('modeloFisherFace.xml')
+face_recognizer.read('modeloLBPHFace.xml')
+
+faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
+
+
+
+
+while(1):
+    
+    cap.open(url) # Antes de capturar el frame abrimos la url
+    
+    ret,frame = cap.read() # Captura de frame
+    if ret == False: break
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    auxFrame = gray.copy()
+
+    faces = faceClassif.detectMultiScale(gray,1.3,5)
+
+    for (x,y,w,h) in faces:
+        rostro = auxFrame[y:y+h,x:x+w]
+        rostro = cv2.resize(rostro,(150,150),interpolation= cv2.INTER_CUBIC)
+        result = face_recognizer.predict(rostro)
+
+        cv2.putText(frame,'{}'.format(result),(x,y-5),1,1.3,(255,255,0),1,cv2.LINE_AA)
+        '''
+		# EigenFaces
+		if result[1] < 5700:
+			cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
+			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
+		else:
+			cv2.putText(frame,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
+			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
+		
+		# FisherFace
+		if result[1] < 500:
+			cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
+			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
+		else:
+			cv2.putText(frame,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
+			cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
+		'''
+		# LBPHFace
+        if result[1] < 70:
+            cv2.putText(frame,'{}'.format(imagePaths[result[0]]),(x,y-25),2,1.1,(0,255,0),1,cv2.LINE_AA)
+            cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
+            ser.write(b'P')
+        else:
+            #ser.write(b'N')
+            cv2.putText(frame,'Desconocido',(x,y-20),2,0.8,(0,0,255),1,cv2.LINE_AA)
+            cv2.rectangle(frame, (x,y),(x+w,y+h),(0,0,255),2)
+    
+    #frame = cv2.rotate(frame,cv2.ROTATE_90_CLOCKWISE)
+    #gris = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow(winName,frame)
+    tecla = cv2.waitKey(1) & 0xFF
+    if tecla == 27:
+	    break
+cap.release()
+cv2.destroyAllWindows()
+ser.close()
---
+++ sketch_FINAL/sketch_FINAL.ino
@@ -0,0 +1,75 @@
+int led1 = 7;
+int led2 = 8;
+int led3 = 12;
+int led4 = 10;
+int led5 = 9;
+int buzzerPin = 11; // Asigna el pin del buzzer
+int option;
+bool executed = false; // Variable para controlar si se ejecutó o no
+unsigned long executionTime = 0; // Variable para controlar el tiempo de espera
+int counter = 0; // Contador para la letra 'N'
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+
+  if (Serial.available() > 0 && !executed) { // Se ejecuta si hay datos disponibles y no se ha ejecutado aún
+    option = Serial.read();
+    Serial.println(option);
+    if (option == 'P') {
+      digitalWrite(led1, LOW);
+      digitalWrite(led2, HIGH);
+      digitalWrite(led3, HIGH);
+      delay(1100);
+      digitalWrite(led1, LOW);
+      digitalWrite(led2, LOW);
+      delay(7000);
+      digitalWrite(led1, HIGH);
+      digitalWrite(led2, LOW);
+      digitalWrite(led3, LOW);
+      delay(1100);
+      digitalWrite(led1, LOW);
+      digitalWrite(led2, LOW);
+      executed = true; // Se marca como ejecutado
+      executionTime = currentMillis; // Se guarda el tiempo actual
+      Serial.end(); // Se cierra el puerto serial
+    } else if (option == 'N') {
+      digitalWrite(led4, HIGH);
+      delay(2000);
+      digitalWrite(led4, LOW);
+      counter++; // Incrementa el contador al recibir 'N'
+
+      // Si el contador llega a 3, activa el led4 y el buzzer
+      if (counter == 3) {
+        digitalWrite(led4, HIGH);
+        tone(buzzerPin, 1000); // Activa el buzzer a 1000Hz
+        counter = 0; // Reinicia el contador
+      }
+      executed = true; // Se marca como ejecutado
+      executionTime = currentMillis; 
+      Serial.end(); 
+    }
+    digitalWrite(led5, HIGH);
+    delay(15000);
+    digitalWrite(led5, LOW);
+  }
+
+  // Si ya se ha ejecutado y han pasado 15 segundos, se reinicia la variable executed
+  if (executed && (currentMillis - executionTime >= 15000)) {
+    executed = false;
+     
+    //counter = 0; // Reinicia el contador
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, LOW);
+    noTone(buzzerPin); // Detiene el sonido del buzzer
+    Serial.begin(9600); 
+  }
+}
